ef66612e8c820b7bd0db76ca873bbb69bd2c3edd
jhj7779 <jhj7779@gmail.com>
2021-06-04 14:23:24 +0900

    BookManager0604
    로그인화면까지 연결완료.

---
+++ DB project/BookManager/BookManager/Form4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookManager
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "1234")
+            {
+                new Form1().ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("비밀번호를 다시 입력하십시오.");
+            }
+        }
+    }
+}
